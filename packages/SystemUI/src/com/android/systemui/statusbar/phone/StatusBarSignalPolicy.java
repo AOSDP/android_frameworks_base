4d07a93591faa7d4b35de371def84b5b9554aa4d
Nico <nicorg2529@gmail.com>
2019-01-07 14:52:08 +0100

    StatusBarSignalPolicy: Fix missing provisioned in equals and copyTo
    * equals and copyTo should include all MobileIconState properties
    * therefore, add the newly introduced boolean, provisioned
    
    Change-Id: Ic810bbe7538506612f86695ce520e8b9542bc634

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -443,6 +443,7 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
                     typeId == that.typeId &&
                     roaming == that.roaming &&
                     needsLeadingPadding == that.needsLeadingPadding &&
+                    provisioned == that.provisioned &&
                     Objects.equals(typeContentDescription, that.typeContentDescription) &&
                     volteId == that.volteId;
         }
@@ -468,6 +469,7 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
             other.typeId = typeId;
             other.roaming = roaming;
             other.needsLeadingPadding = needsLeadingPadding;
+            other.provisioned = provisioned;
             other.typeContentDescription = typeContentDescription;
             other.volteId = volteId;
         }
